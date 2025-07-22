1090b957a16e88206061abd88ef13c7629f3bde7
Kuro <kuro@MacBook-Air-Kuro.local>
2025-07-22 14:36:15 +0300

    fixed migr

--- db/migrations/20250624205600_create_carts_table.up.sql
+++ db/migrations/20250624205600_create_carts_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE carts (
                        id SERIAL PRIMARY KEY,
-                       user_id INT
+                       user_id INT,
+                       status TEXT NOT NULL DEFAULT 'active'
 
 );
-
