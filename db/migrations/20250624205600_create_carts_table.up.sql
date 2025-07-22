fd0cbd4eb0093751e023c7e5bcffbb4b82ce3b71
Kuro <kuro@MacBook-Air-Kuro.local>
2025-07-23 01:23:01 +0300

    fixed all

--- db/migrations/20250624205600_create_carts_table.up.sql
+++ db/migrations/20250624205600_create_carts_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE carts (
                        id SERIAL PRIMARY KEY,
                        user_id    UUID NOT NULL
                            REFERENCES users(id)
-                               ON DELETE CASCADE,,
+                               ON DELETE CASCADE,
                        status TEXT NOT NULL DEFAULT 'active'
 
 );
